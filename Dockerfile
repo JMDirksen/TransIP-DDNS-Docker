3c3406bbd5aa0a19f2515f6f2234a094a47f266c
JMDirksen <jeftadirksen@gmail.com>
2023-11-11 15:02:28 +0100

    Init

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM php:cli
+WORKDIR /root
+COPY --chmod=700 run.sh .
+ADD --chmod=700 https://github.com/transip/tipctl/releases/latest/download/tipctl.phar tipctl.phar
+RUN cp /usr/local/etc/php/php.ini-production /usr/local/etc/php/php.ini
+
+ENV LOGINNAME=myusername
+ENV PRIVATEKEY=myprivatekey
+ENV DOMAIN=mydomain.com
+ENV RECORD=@
+ENV TTL=300
+ENV TYPE=A
+ENV INTERVAL=300
+
+CMD ./run.sh
---
+++ run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+./tipctl.phar setup -n --apiUseWhitelist=false --loginName="$LOGINNAME" --apiPrivateKey="$PRIVATEKEY" -vvv
+LastSet="?"
+
+while :
+do
+    CurrentIP="$(curl -s ipinfo.io/ip)"
+    if [ "$CurrentIP" != "$LastSet" ]
+    then
+        ./tipctl.phar domain:dns:updatednsentry $DOMAIN $RECORD $TTL $TYPE $CurrentIP
+        LastSet=$CurrentIP
+        echo "Set $RECORD.$DOMAIN -> $CurrentIP"
+    fi
+
+    sleep $INTERVAL
+done
